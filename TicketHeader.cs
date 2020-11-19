1ecb22d9652f75afec20250c0adf93fe0c7ab83d
deko <67808387+dekofr@users.noreply.github.com>
2020-11-19 13:20:19 +0100

    Add files via upload

---
+++ TicketHeader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dex0fame
+{
+    class TicketHeader
+    {
+        public string anyAttribute { get; set; }
+        public string Ticket { get; set; }
+    }
+}
